32dffde9c4f97fcfd7bf7e1ecf286bd2aab3aaeb
Shad <shadmirza100@gmail.com>
2022-04-29 18:36:29 +0530

    backend added for contact form , response email template added

--- iet_cdoe/settings.py
+++ iet_cdoe/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST_USER = "web.cdoe@ietdavv.edu.in"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'ietdavv22'
--- main/views.py
+++ main/views.py
@@ -1,7 +1,29 @@
+import email
+import imp
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        response_email = render_to_string('response_email.html', {'name': name})
+        mail = EmailMultiAlternatives('Thanks for response', response_email, settings.EMAIL_HOST_USER, [email])
+        mail.content_subtype = 'html'
+        mail.send()
+
+        send_mail('Message from CDOE Contact Form | Subject: '+subject, 'Name: '+name+'\n'+'Email: '+email+'\n'+'Subject: '+subject+'\n'+'Message: '+message, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
+        return HttpResponseRedirect('contact')
+
+    else:
+        return render(request, 'contact.html')
