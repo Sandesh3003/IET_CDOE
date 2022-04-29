84e62e46d5437c45db6790741d1ca32170430ad7
Shad <shadmirza100@gmail.com>
2022-04-29 15:04:15 +0530

    fixed contact page and about section styling

--- iet_cdoe/urls.py
+++ iet_cdoe/urls.py
@@ -19,6 +19,6 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
     path('contact', views.contact, name="contact")
 ]
