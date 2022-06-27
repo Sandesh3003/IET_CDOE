36279ec6502b3464e120240faaf121c5bbdc39e5
Shad <shadmirza100@gmail.com>
2022-06-27 22:36:40 +0530

    .

--- iet_cdoe/settings.py
+++ iet_cdoe/settings.py
@@ -93,7 +93,8 @@ if os.environ.get('DEBUG') == '0' :
             'NAME': os.environ.get('DB_NAME'),  
             'USER': os.environ.get('DB_USER'),  
             'PASSWORD': os.environ.get('DB_PASSWORD'),  
-            'HOST': os.environ.get('DB_HOST'),  
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
             'OPTIONS': {  
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
             }  
