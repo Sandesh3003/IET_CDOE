b4702006e0e1e0d29736b75627340855fcc9da11
Shad <shadmirza100@gmail.com>
2022-06-28 00:04:04 +0530

    .

--- iet_cdoe/settings.py
+++ iet_cdoe/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'iet_cdoe.wsgi.application'
 #     }  
 
 # else :
-      DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
