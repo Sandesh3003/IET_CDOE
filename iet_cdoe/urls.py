30ac58b0288cc73dc682e2627caa4905742ed749
Shad Mirza <shadmirza100@gmail.com>
2022-10-18 12:56:51 +0530

    .

--- iet_cdoe/urls.py
+++ iet_cdoe/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
 from main import views
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
